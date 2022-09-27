41a3229dc24189c82bf786779adb1b751b9d4827
BOg <timohamaluh.2005@mail.ru>
2022-09-27 21:06:29 +0300

    Add lesson 3 work 5 js

--- Lesson-3/index.js
+++ Lesson-3/index.js
@@ -4,22 +4,37 @@
 }
 for(let i = 35; i >= 8; i--){
     console.log(i);
-}*/
+}
 //Второе задание
-/*let i = 89;
+let i = 89;
 while(i >= 11){
     console.log(i);
     i--;
-}*/
+
 //Третье задание
-/*let sum = 0;
+let sum = 0;
 for(let i = 0; i <= 100; i++){
     sum += i;
 }
-console.log(sum);*/
+console.log(sum);
 //Четвертое задание
 let sum = 0;
 for(let i = 0; i <= 5; i++){
     sum += i;
     console.log(sum);
+}*/
+//Пятое задание
+//while
+let i = 8;
+while(i <= 56){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+    i++;
+}
+//for
+for(i = 8; i <= 56; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
 }
