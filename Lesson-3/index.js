e0717220823d0166fe2de0fc4ff71790bc532b2d
BOg <timohamaluh.2005@mail.ru>
2022-09-27 20:54:54 +0300

    Add lesson 3 work 2 js

--- Lesson-3/index.js
+++ Lesson-3/index.js
@@ -1,7 +1,13 @@
 //Первое задание
-for(let i = 1; i <= 50; i++){
+/*for(let i = 1; i <= 50; i++){
     console.log(i);
 }
 for(let i = 35; i >= 8; i--){
     console.log(i);
+}*/
+//Второе задание
+let i = 89;
+while(i >= 11){
+    console.log(i);
+    i--;
 }
