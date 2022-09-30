c37a4dbd6b850025d5e0ae6671ddba43a81e2e60
BOg <timohamaluh.2005@mail.ru>
2022-09-30 19:32:06 +0300

    Add lesson 5 work 2 js

--- Lesson-5/index.js
+++ Lesson-5/index.js
@@ -1,4 +1,12 @@
-function work(x1,x2,x3){
+//Первое задание
+/*function work(x1,x2,x3){
     return (x1 - x2) / x3
 }
-console.log(work(19,2,9))
+console.log(work(19,2,9))*/
+//Второе задание
+function degree(x){
+    y = x**3;
+    y1 = x**2;
+    return 'Куб числа = ' + String(y) + '.Квадрат числа = ' + String(y1) 
+}
+console.log(degree(10));
