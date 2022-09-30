72fd046a89c4f06fdd6cfcba007585cf9222ed78
BOg <timohamaluh.2005@mail.ru>
2022-09-30 19:41:29 +0300

    Add lesson 5 work 3 js

--- Lesson-5/index.js
+++ Lesson-5/index.js
@@ -4,9 +4,22 @@
 }
 console.log(work(19,2,9))*/
 //Второе задание
-function degree(x){
+/*function degree(x){
     y = x**3;
     y1 = x**2;
     return 'Куб числа = ' + String(y) + '.Квадрат числа = ' + String(y1) 
 }
-console.log(degree(10));
+console.log(degree(10));*/
+//Третье задание
+let a = 10,
+    b = 15;
+function min(x1,x2){
+    if(x1 > x2) return 'Меньшее из двух чисел b = ' + String(x2);
+    else return 'Меньшее из двух чисел a = ' + String(x1);
+}
+function max(x1,x2){
+    if(x1 > x2) return 'Большее из двух чисел a = ' + String(x1);
+    else return 'Большее из двух чисел b = ' + String(x2);
+}
+console.log(min(a,b));
+console.log(max(a,b));
