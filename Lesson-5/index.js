1f4779fecae4ebce19a02b4005d6b44184d0166e
BOg <timohamaluh.2005@mail.ru>
2022-09-30 19:48:27 +0300

    Add lesson 5 work 4 js

--- Lesson-5/index.js
+++ Lesson-5/index.js
@@ -11,7 +11,7 @@ console.log(work(19,2,9))*/
 }
 console.log(degree(10));*/
 //Третье задание
-let a = 10,
+/*let a = 10,
     b = 15;
 function min(x1,x2){
     if(x1 > x2) return 'Меньшее из двух чисел b = ' + String(x2);
@@ -22,4 +22,20 @@ function max(x1,x2){
     else return 'Большее из двух чисел b = ' + String(x2);
 }
 console.log(min(a,b));
-console.log(max(a,b));
+console.log(max(a,b));*/
+//Четвертое задание
+let a = [];
+function input(x1,x2){
+    x1 = prompt('Введите первое число диапазона')
+    x2 = prompt('Введите последнее число диапазона')
+    for(let i = x1; i <= x2; i++){
+        a.push(i);
+    }
+}
+function output(){
+    for(let i = 0; i < a.length; i++){
+        console.log(a[i]);
+    }
+}
+input(10,15);
+output();
