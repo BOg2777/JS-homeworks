efe53fb19275681d10279db9669a34877a6ee086
BOg <timohamaluh.2005@mail.ru>
2022-10-02 18:30:11 +0300

    Add lesson 5 work 8-13(8,10,12,13 не сделаны) js

--- Lesson-5/index.js
+++ Lesson-5/index.js
@@ -62,7 +62,7 @@ arr();
 ar();
 console.log(arr2);*/
 //Седьмое задание
-function pyramid(x1,x2){
+/*function pyramid(x1,x2){
     if(x2 == null){
         for(let i = 1; i <= x1; i++){
             for(let j = 1; j <= i; j++){
@@ -79,4 +79,80 @@ function pyramid(x1,x2){
         }
     }
 }
-pyramid(9,'+');
+pyramid(9,'+');*/
+//-Восьмое задание
+/*function pyramid(x1){
+    for(let i = 1; i <= x1; i++){
+        for(let j = 1; j <= i; j++){
+            document.write('*');
+        }
+        document.write('</br>');
+    }
+}
+pyramid(2);*/
+//Девятое задание
+/*function Fibonachi(){
+    let arr = [1,1],
+        x1 = 1,
+        x2 = 1,
+        x3 = 0;
+    for(let i = 0; i < 1000; i = x3){
+        x3 =x1 + x2;
+        x1 = x2;
+        x2 = x3; 
+        arr.push(x3);
+    }
+    arr.length = arr.length - 1;
+    console.log(arr);
+}
+Fibonachi();*/
+//-Десятое задание
+//ОДиннадцатое задание
+/*let i = 0;
+function arr(x){
+    if(i != x.length){
+        console.log(x[i]);
+        i++;
+        arr(x);
+    }
+}
+arr([12,32,512,'qwe']);*/
+//-Двенадцатое задание
+/*function data(){
+    let name = prompt('Введите имя'),
+        secondname = prompt('Введите фамилию'),
+        thirdname = prompt('Введите отчество'),
+        group = prompt('Введите вашу группу'),
+        firststr = 'Домашняя работа "Функции"',
+        secondstr = 'Выполнил: студент группы ' + group,
+        thirdstr = name + ' ' + secondname + ' ' + thirdname,
+         i = 0;
+    if(firststr.length >= secondstr.length && firststr.length >= thirdstr.length){
+        i = firststr.length;
+    }else if(secondstr.length >= firststr.length && secondstr.length >= thirdname.length){
+        i = secondstr.length;
+    }else{
+        i = thirdstr.length;
+    }
+    for(let z = 0; z <= i+3; z++){
+        document.write('*');
+    }
+    document.write('</br>');
+    document.write('* ' + firststr);
+    // for(let z = firststr.length; z <= i+3; z++){
+    //     document.write('+');
+    // }
+    document.write('*');
+    document.write('</br>');
+    document.write('* ' + secondstr);
+    document.write('*');
+    document.write('</br>');
+    document.write('* ' + thirdstr );
+    document.write('*');
+    document.write('</br>');
+    for(let z = 0; z <= i+3; z++){
+        document.write('*');
+    }
+}
+data();*/
+//-Тринадцатое задание
