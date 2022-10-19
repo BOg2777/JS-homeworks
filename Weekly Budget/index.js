cb309fa037559ba0c1fa7689be78bef3b4d5d8f2
BOg <timohamaluh.2005@mail.ru>
2022-10-19 21:04:52 +0300

    Add Weekly Budget localstorage js

--- Weekly Budget/index.js
+++ Weekly Budget/index.js
@@ -8,6 +8,13 @@ let inputName = document.getElementById('mainInputName');
 let inputPrice = document.getElementById('mainInputPrice');
 let shoppingList = document.getElementById('shoppingList');
 let mainText = document.getElementById('mainText');
+let parentHisoryList = document.getElementById('mainRight');
+let budgetList = [];
+
+if(localStorage.getItem('budget') != undefined){
+    budgetList = JSON.parse(localStorage.getItem('budget'));
+    StartAddBudget();
+}
 
 startBtn.addEventListener('click', addBudget);
 
@@ -22,10 +29,12 @@ function addBudget () {
     }else{
         alert('Введите свой бюджет');
     }
+    
 }
 function countBudget () {
     name = String(inputName.value);
     price = Number(inputPrice.value);
+    
     if(name != ''){
         budgetValue = budgetValue - price;
         priceAll += price;
@@ -50,4 +59,22 @@ function addNamePrice() {
     p.classList.add('buyList');
     p.innerHTML = name + ': $' + price;
     shoppingList.appendChild(p);
+    let NamePiceObj = {
+        name: name,
+        price: price
+    } 
+    budgetList.push(NamePiceObj);
+    localStorage.setItem('budget', JSON.stringify(budgetList));
+}
+function StartAddBudget (){
+    let historyList = document.createElement('div');
+    historyList.classList.add('buyList');
+    historyList.innerHTML = `<p class='buyList'>History:</p>`;
+    parentHisoryList.appendChild(historyList);
+    for(let key in budgetList){
+        let p = document.createElement('p');
+        p.classList.add('buyList');
+        p.innerHTML = budgetList[key].name + ': $' + budgetList[key].price;
+        historyList.appendChild(p);
+    }
 }
