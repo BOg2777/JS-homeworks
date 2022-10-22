e3a348a93718189a60da2ccffe9c74a2e4702253
BOg <timohamaluh.2005@mail.ru>
2022-10-22 16:47:41 +0300

    Add quizeApp html scc and some js

---
+++ quizeApp/index.js
@@ -0,0 +1,6 @@
+let question = document.getElementById('question');
+let answersWrapper = document.getElementById('answers');
+let answer1 = document.getElementById('answer1');
+let answer2 = document.getElementById('answer2');
+let answer3 = document.getElementById('answer3');
+let answer4 = document.getElementById('answer4');
